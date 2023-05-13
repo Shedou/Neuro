93435ca57d819205e8fd067185362147cdeb62d1
Chimbal <chimbal.chimbal@yandex.ru>
2023-05-13 15:16:13 +0400

    Update v2

---
+++ SD_WEBUI_Neuro/WebUI CUDA LowVRAM NoFP16.bat
@@ -0,0 +1,20 @@
+@echo off
+set DIR=%~dp0Depends
+cd webui\
+set PATH=C:\Windows\system32;C:\Windows;%DIR%\git\bin;%DIR%\python310;%DIR%\python310\Scripts;%DIR%\ffmpeg-6.0-full_build-shared\bin
+set TRANSFORMERS_CACHE=%DIR%\transformers-cache
+
+set GIT_SSL_NO_VERIFY=true
+
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS=--precision full --no-half --lowvram --skip-torch-cuda-test
+
+: Fix venv.
+copy /y NUL "%~dp0webui\venv\pyvenv.cfg"
+echo home = %DIR%\python310>>"%~dp0webui\venv\pyvenv.cfg"
+echo include-system-site-packages = false>>"%~dp0webui\venv\pyvenv.cfg"
+echo version = 3.10.6>>"%~dp0webui\venv\pyvenv.cfg"
+
+call webui.bat
